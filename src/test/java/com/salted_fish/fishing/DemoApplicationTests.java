de324b0e5e926dbb99ac49c4ca2162f12d353bd1
qingyuxu <1010486983@qq.com>
2019-08-23 12:40:48 +0800

    first commit configure spring boot environment

---
+++ src/test/java/com/salted_fish/fishing/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salted_fish.fishing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
